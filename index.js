a637392a5577214922274aa03390713ebb454a8a
Corey Ladovsky <coreyskymusic@gmail.com>
2021-02-15 07:57:37 -0800

    add styles palindrom and favorite numbers

--- index.js
+++ index.js
@@ -6,9 +6,63 @@ const disemvowel = () => {
     let output = "";
     const vowels = "aeiou"
     for(const char of str) {
-        if(!vowels.includes(char)) {
+        if(!vowels.includes(char.toLowerCase())) {
             output += char;
         }
     }
     p.textContent = output;
 }
+
+const isPalindrome = () => {
+    const str = document.querySelector("#is-palindrome-input").value;
+    const p = document.querySelector("#is-palindrome-p");
+    let result = "true"; 
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] !== str[str.length - i - 1]) {
+            result = "false"
+        }
+    }
+    p.textContent = result;
+}
+const sum = (arr) => {
+    let sum = 0; 
+    arr.forEach(num => {
+        sum += num
+    })
+    return sum; 
+}
+const average = (arr) => {
+    let sumOfNums = sum(arr) 
+    return sumOfNums / arr.length; 
+}
+
+const onlyOdds = arr => {
+    return arr.filter(num => num % 2 === 1)
+}
+
+const favoriteNumbers = () => {
+    let list = document.querySelectorAll("#favorite-numbers > li");
+    let arr = [];
+    list.forEach((listItem) => {
+        arr.push(Number(listItem.textContent))
+    })
+    const sumOfNums = sum(arr);
+    const averageOfNums = average(arr);
+    const onlyOddsOfNums = onlyOdds(arr);
+
+    const sumOfFav = document.querySelector("#sum-of-favorite-nums");
+    sumOfFav.textContent += sumOfNums; 
+
+    const aveOfFav = document.querySelector("#average-of-favorite-nums");
+    aveOfFav.textContent += averageOfNums; 
+
+    const oddList = document.querySelector("#list-of-odd-favorite-nums");
+    
+    onlyOddsOfNums.forEach((odd) => {
+      const li = document.createElement("li");
+      li.innerText = odd;
+      oddList.appendChild(li);
+    });
+}
+
+favoriteNumbers();
